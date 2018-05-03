73fa26568cdbc971ee4b9a8b380772545a56ed7d
Guy Bar Yosef <guybaryosef@gmail.com>
2018-05-02 23:13:42 -0400

    added rinkeby funcitonality

--- dApp/truffle.js
+++ dApp/truffle.js
@@ -1,11 +1,18 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
   networks: {
+      // See <http://truffleframework.com/docs/advanced/configuration>
+      // for more about customizing your Truffle configuration!
     development: {
-      host: "127.0.0.1",
+      host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+    },
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0xBBAFad45E1ae07285Bf5627D810065859dc318e8", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 2692389 // Gas limit used for deploys
     }
   }
-}
+};
